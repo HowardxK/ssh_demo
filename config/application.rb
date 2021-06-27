9f280d8c8491f679a5ab6dcecddb8340817c469e
howard <howard67890@gmail.com>
2021-06-27 10:34:56 +0800

    首頁 layout navbar

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module MyCitiesocial
     config.generators do |g|
       g.assets false
       g.helper false
+      g.test_framework false
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
