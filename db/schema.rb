5636e5d924ec50ba80a4b8cbd9e375cbf73e0abd
howard <howard67890@gmail.com>
2021-06-29 18:16:13 +0800

    廠商管理 part 1(TDD)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_023517) do
+ActiveRecord::Schema.define(version: 2021_06_29_100803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,14 +148,11 @@ ActiveRecord::Schema.define(version: 2021_06_29_023517) do
     t.boolean "online", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_vendors_on_deleted_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "skus"
   add_foreign_key "products", "categories"
-  add_foreign_key "products", "vendors"
   add_foreign_key "skus", "products"
 end
