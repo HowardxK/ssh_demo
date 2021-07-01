570870377d01ed569e1338dcdd5e82f1a672757c
howard <howard67890@gmail.com>
2021-07-01 14:48:02 +0800

    商品管理 part 3

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,4 +5,26 @@ class Admin::ProductsController < Admin::BaseController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path, notice: '商品已新增'
+    else
+      render :new
+    end
+  end
+
+  private
+  def find_product
+    @product = Product.friendly_id.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, 
+                                    :vendor_id,
+                                    :list_price, 
+                                    :sell_price)
+  end
 end
