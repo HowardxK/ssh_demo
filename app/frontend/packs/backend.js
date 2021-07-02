11a95aee42250f1762ec3d8158d177c32531c9b5
howard <howard67890@gmail.com>
2021-07-02 11:54:41 +0800

    商品說明（上傳圖片修正）

--- app/frontend/packs/backend.js
+++ app/frontend/packs/backend.js
@@ -2,6 +2,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
+// enable action text
+require("trix")
+require("@rails/actiontext")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
