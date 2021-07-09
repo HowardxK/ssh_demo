225c311762cdeac052e6efcd004ab4f8b3c34292
howard <howard67890@gmail.com>
2021-07-09 12:07:26 +0800

    購物車 part 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,16 @@
 # PORO = Plain Old Ruby Object
 class Cart
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
   def add_item(product_id)
+    @items << product_id
   end
 
   def empty?
-    false
+    @items.empty?     # 陣列內建 empty? 方法
   end
 end
