be104214d31cf95e946767311065267b7e5d32f8
howard <howard67890@gmail.com>
2021-07-22 10:46:32 +0800

    串接 LINE Pay part 3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,19 @@ class OrdersController < ApplicationController
     end
 
     if @order.save
+      resp = Faraday.post("#{ENV['line_pay_endpoint']}/v2/payments/request") do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['X-LINE-ChannelId'] = ENV['line_pay_channel_id']
+        req.headers['X-LINE-ChannelSecret'] = ENV['line_pay_channel_secret']
+        req.body = {
+          productName: "Zelda",
+          amount: current_cart.total_price.to_i,
+          currency: "TWD",
+          confirmUrl: "http://localhost:3000/orders/confirm",
+          orderId: @order.num
+        }.to_json
+      end
+      
       redirect_to root_path, notice: 'OK'
     else
       render 'carts/checkout'
